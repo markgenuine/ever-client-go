e9cf8a4b4ffe3f5a2fdab2ab2645ed81183ec6d8
markgenuine <crazygroup.corp@gmail.com>
2020-08-08 16:28:37 +0300

    delete chaID parameters
    func "ParseConfig" only read config-file user

---
+++ tools.go
@@ -0,0 +1,15 @@
+package goton
+
+import (
+	"github.com/BurntSushi/toml"
+)
+
+//ParseConfigFile parse TOML config file
+func ParseConfigFile(path string) (*TomlConfig, error) {
+	var config TomlConfig
+	if _, err := toml.DecodeFile(path, &config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
