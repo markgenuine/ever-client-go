bb143c269565c96f05e524a45430ccf7be5cac2d
markgenuine <crazygroup.corp@gmail.com>
2020-11-19 03:24:26 +0300

    feat: change usecase and tests

--- usecase/net/net.go
+++ usecase/net/net.go
@@ -21,21 +21,27 @@ func NewNet(
 }
 
 // QueryCollection method net.query_collection
-func (n *net) QueryCollection(pOQC domain.ParamsOfQueryCollection) (int, error) {
-	return n.client.Request("net.query_collection", pOQC)
+func (n *net) QueryCollection(pOQC domain.ParamsOfQueryCollection) (*domain.ResultOfQueryCollection, error) {
+	result := new(domain.ResultOfQueryCollection)
+	err := n.client.GetResult("net.query_collection", pOQC, result)
+	return result, err
 }
 
 // WaitForCollection net.wait_for_collection
-func (n *net) WaitForCollection(pOWFC domain.ParamsOfWaitForCollection) (int, error) {
-	return n.client.Request("net.wait_for_collection", pOWFC)
+func (n *net) WaitForCollection(pOWFC domain.ParamsOfWaitForCollection) (*domain.ResultOfWaitForCollection, error) {
+	result := new(domain.ResultOfWaitForCollection)
+	err := n.client.GetResult("net.wait_for_collection", pOWFC, result)
+	return result, err
 }
 
 // Unsubscribe net.unsubscribe
-func (n *net) Unsubscribe(rOSC domain.ResultOfSubscribeCollection) (int, error) {
-	return n.client.Request("net.unsubscribe", rOSC)
+func (n *net) Unsubscribe(rOSC domain.ResultOfSubscribeCollection) {
+	n.client.GetResult("net.unsubscribe", rOSC, nil)
 }
 
 // SubscribeCollection method net.subscribe_collection
-func (n *net) SubscribeCollection(pOSC domain.ParamsOfSubscribeCollection) (int, error) {
-	return n.client.Request("net.subscribe_collection", pOSC)
+func (n *net) SubscribeCollection(pOSC domain.ParamsOfSubscribeCollection) (*domain.ResultOfSubscribeCollection, error) {
+	result := new(domain.ResultOfSubscribeCollection)
+	err := n.client.GetResult("net.subscribe_collection", pOSC, result)
+	return result, err
 }
--- usecase/net/net_test.go
+++ usecase/net/net_test.go
@@ -1,1 +1,76 @@
 package net
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/move-ton/ton-client-go/domain"
+	"github.com/move-ton/ton-client-go/gateway/client"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNet(t *testing.T) {
+
+	client, err := client.NewClientGateway(domain.NewDefaultConfig(2))
+	assert.Equal(t, nil, err)
+	defer client.Destroy()
+
+	netUC := net{
+		config: domain.NewDefaultConfig(2),
+		client: client,
+	}
+	defer netUC.client.Destroy()
+
+	t.Run("TestQueryCollection", func(t *testing.T) {
+		valueRes1, err := netUC.QueryCollection(domain.ParamsOfQueryCollection{Collection: "blocks_signatures", Result: "id", Limit: 1})
+		assert.Equal(t, nil, err)
+		assert.Greater(t, len(valueRes1.Result), 0)
+
+		valueRes2, err := netUC.QueryCollection(domain.ParamsOfQueryCollection{Collection: "accounts", Result: "id, balance", Limit: 5})
+		assert.Equal(t, nil, err)
+		assert.Equal(t, 5, len(valueRes2.Result))
+
+		valueRes3, err := netUC.QueryCollection(domain.ParamsOfQueryCollection{Collection: "messages", Filter: json.RawMessage(`{"created_at":{"gt":1562342740}}`), Result: "body, created_at", Order: []domain.OrderBy{{Path: "created_at", Direction: domain.SortDirectionASC}}, Limit: 10})
+		assert.Equal(t, nil, err)
+		var (
+			objmap    map[string]json.RawMessage
+			createdAt int
+		)
+		err = json.Unmarshal(valueRes3.Result[0], &objmap)
+		assert.Equal(t, nil, err)
+		bytesRes, err := objmap["created_at"].MarshalJSON()
+		assert.Equal(t, nil, err)
+		err = json.Unmarshal(bytesRes, &createdAt)
+		assert.Equal(t, nil, err)
+		assert.Greater(t, createdAt, 1562342740)
+
+		_, err = netUC.QueryCollection(domain.ParamsOfQueryCollection{Collection: "messages"})
+		assert.NotEqual(t, nil, err)
+	})
+
+	t.Run("TestWaitCollection", func(t *testing.T) {
+		nowTime := int(time.Now().Unix())
+		filter := fmt.Sprintf(`{"now":{"gt":%d}}`, nowTime)
+		query := domain.ParamsOfWaitForCollection{Collection: "transactions", Filter: json.RawMessage(filter), Result: "id, now"}
+		valueRes1, err := netUC.WaitForCollection(query)
+		assert.Equal(t, nil, err)
+		var (
+			objmap  map[string]json.RawMessage
+			dateNow int
+		)
+		err = json.Unmarshal(valueRes1.Result, &objmap)
+		assert.Equal(t, nil, err)
+		bytesNow, err := objmap["now"].MarshalJSON()
+		assert.Equal(t, nil, err)
+		err = json.Unmarshal(bytesNow, &dateNow)
+		assert.Equal(t, nil, err)
+		assert.Greater(t, dateNow, nowTime)
+
+		query.Timeout = 1
+		valueRes2, err := netUC.WaitForCollection(query)
+		assert.NotEqual(t, nil, err)
+		assert.Equal(t, json.RawMessage(nil), valueRes2.Result)
+	})
+}
