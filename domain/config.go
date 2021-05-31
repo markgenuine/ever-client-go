929599005c6e329ac4085617a7dbeb05b7b50d89
markgenuine <crazygroup.corp@gmail.com>
2021-05-31 13:09:11 +0300

    feat: change config in domain

--- domain/config.go
+++ domain/config.go
@@ -1,9 +1,11 @@
 package domain
 
-const(
-	BaseUrl = "https://net.ton.dev/"
+import "github.com/move-ton/ton-client-go/util"
+
+const (
+	BaseUrl       = "https://net.ton.dev/"
 	BaseCustomUrl = "https://tonos.freeton.surf"
-	BaseMainUrl = "https://main.ton.dev"
+	BaseMainUrl   = "https://main.ton.dev"
 )
 
 type (
@@ -19,34 +21,34 @@ type (
 	Network struct {
 		ServerAddress            string   `toml:"server_address" json:"server_address,omitempty"`
 		Endpoints                []string `toml:"endpoints" json:"endpoints,omitempty"`
-		NetworkRetriesCount      int      `toml:"network_retries_count" json:"network_retries_count,omitempty"`
-		MaxReconnectTimeOut      int      `toml:"max_reconnect_timeout" json:"max_reconnect_timeout,omitempty"`
-		ReconnectTimeout         int      `toml:"reconnect_timeout" json:"reconnect_timeout,omitempty"`
-		MessageRetriesCount      int      `toml:"message_retries_count" json:"message_retries_count,omitempty"`
-		MessageProcessingTimeout int      `toml:"message_processing_timeout" json:"message_processing_timeout,omitempty"`
-		WaitForTimeout           int      `toml:"wait_for_timeout" json:"wait_for_timeout,omitempty"`
-		OutOfSyncThreshold       int      `toml:"out_of_sync_threshold" json:"out_of_sync_threshold,omitempty"`
-		SendingEndpointCount     *int      `toml:"sending_endpoint_count,omitempty" json:"sending_endpoint_count"`
+		NetworkRetriesCount      *int     `toml:"network_retries_count" json:"network_retries_count,omitempty"`
+		MaxReconnectTimeOut      *int     `toml:"max_reconnect_timeout" json:"max_reconnect_timeout,omitempty"`
+		ReconnectTimeout         *int     `toml:"reconnect_timeout" json:"reconnect_timeout,omitempty"`
+		MessageRetriesCount      *int     `toml:"message_retries_count" json:"message_retries_count,omitempty"`
+		MessageProcessingTimeout *int     `toml:"message_processing_timeout" json:"message_processing_timeout,omitempty"`
+		WaitForTimeout           *int     `toml:"wait_for_timeout" json:"wait_for_timeout,omitempty"`
+		OutOfSyncThreshold       *int     `toml:"out_of_sync_threshold" json:"out_of_sync_threshold,omitempty"`
+		SendingEndpointCount     *int     `toml:"sending_endpoint_count,omitempty" json:"sending_endpoint_count"`
 		AccessKey                string   `toml:"access_key" json:"access_key,omitempty"`
 	}
 
 	// Crypto ...
 	Crypto struct {
-		MnemonicDictionary  int    `toml:"mnemonic_dictionary" json:"mnemonic_dictionary,omitempty"`
-		MnemonicWordCount   int    `toml:"mnemonic_word_count" json:"mnemonic_word_count,omitempty"`
+		MnemonicDictionary  *int   `toml:"mnemonic_dictionary" json:"mnemonic_dictionary,omitempty"`
+		MnemonicWordCount   *int   `toml:"mnemonic_word_count" json:"mnemonic_word_count,omitempty"`
 		HdKeyDerivationPath string `toml:"hdkey_derivation_path" json:"hdkey_derivation_path,omitempty"`
 	}
 
 	// AbiConfig ...
 	AbiConfig struct {
-		WorkChain                          int     `toml:"workchain" json:"workchain,omitempty"`
-		MessageExpirationTimeout           int     `toml:"message_expiration_timeout" json:"message_expiration_timeout"`
-		MessageExpirationTimeoutGrowFactor float32 `toml:"message_expiration_timeout_grow_factor" json:"message_expiration_timeout_grow_factor"`
+		WorkChain                          *int     `toml:"workchain" json:"workchain,omitempty"`
+		MessageExpirationTimeout           *int     `toml:"message_expiration_timeout" json:"message_expiration_timeout,omitempty"`
+		MessageExpirationTimeoutGrowFactor *float32 `toml:"message_expiration_timeout_grow_factor" json:"message_expiration_timeout_grow_factor,omitempty"`
 	}
 
 	// BocConfig ...
 	BocConfig struct {
-		CacheMaxSize int `toml:"cache_max_size" json:"cache_max_size,omitempty"`
+		CacheMaxSize *int `toml:"cache_max_size" json:"cache_max_size,omitempty"`
 	}
 )
 
@@ -54,25 +56,25 @@ type (
 func NewDefaultConfig(address string) Config {
 	config := Config{
 		Network: &Network{
-			ServerAddress:           address,
-			NetworkRetriesCount:      5,
-			MessageRetriesCount:      5,
-			MessageProcessingTimeout: 40000, //ms
-			WaitForTimeout:           40000, //ms
-			OutOfSyncThreshold:       15000, //ms
+			ServerAddress:            address,
+			NetworkRetriesCount:      util.IntToPointerInt(5),
+			MessageRetriesCount:      util.IntToPointerInt(5),
+			MessageProcessingTimeout: util.IntToPointerInt(40000), //ms
+			WaitForTimeout:           util.IntToPointerInt(40000), //ms
+			OutOfSyncThreshold:       util.IntToPointerInt(15000), //ms
 			AccessKey:                "",
 		},
 		Crypto: &Crypto{
-			MnemonicDictionary:  1,
-			MnemonicWordCount:   12,
+			MnemonicDictionary:  util.IntToPointerInt(1),
+			MnemonicWordCount:   util.IntToPointerInt(12),
 			HdKeyDerivationPath: "",
 		},
 		Abi: &AbiConfig{
-			WorkChain:                          0,
-			MessageExpirationTimeout:           40000, //ms
-			MessageExpirationTimeoutGrowFactor: 1.5},
+			WorkChain:                          util.IntToPointerInt(0),
+			MessageExpirationTimeout:           util.IntToPointerInt(40000), //ms
+			MessageExpirationTimeoutGrowFactor: util.Float32ToPointerFloat32(1.5)},
 		Boc: &BocConfig{
-			CacheMaxSize: 10,
+			//CacheMaxSize: util.IntToPointerInt(10),
 		},
 	}
 
@@ -80,21 +82,21 @@ func NewDefaultConfig(address string) Config {
 }
 
 // WordCountList list length mnemonic phrases
-func WordCountList() map[int]int {
-	return map[int]int{12: 12, 15: 15, 18: 18, 21: 21, 24: 24}
+func WordCountList() map[int]*int {
+	return map[int]*int{12: util.IntToPointerInt(12), 15: util.IntToPointerInt(15), 18: util.IntToPointerInt(18), 21: util.IntToPointerInt(21), 24: util.IntToPointerInt(24)}
 }
 
 // DictionaryList list dictionary mnemonic phrase
-func DictionaryList() map[string]int {
-	return map[string]int{
-		"TON":                 0,
-		"ENGLISH":             1,
-		"CHINESE_SIMPLIFIED":  2,
-		"CHINESE_TRADITIONAL": 3,
-		"FRENCH":              4,
-		"ITALIAN":             5,
-		"JAPANESE":            6,
-		"KOREAN":              7,
-		"SPANISH":             8,
+func DictionaryList() map[string]*int {
+	return map[string]*int{
+		"TON":                 util.IntToPointerInt(0),
+		"ENGLISH":             util.IntToPointerInt(1),
+		"CHINESE_SIMPLIFIED":  util.IntToPointerInt(2),
+		"CHINESE_TRADITIONAL": util.IntToPointerInt(3),
+		"FRENCH":              util.IntToPointerInt(4),
+		"ITALIAN":             util.IntToPointerInt(5),
+		"JAPANESE":            util.IntToPointerInt(6),
+		"KOREAN":              util.IntToPointerInt(7),
+		"SPANISH":             util.IntToPointerInt(8),
 	}
 }
